407d51e7eba9ee448b3ab254294e2fbebc23530b
Brent Gardner <bgardner@squarelabs.net>
2022-06-06 19:41:19 -0600

    [WIP] Can read keys dynamically, but not columns yet

--- src/ignite_exec.rs
+++ src/ignite_exec.rs
@@ -1,7 +1,7 @@
 use std::any::Any;
 use std::fmt::{Debug, Formatter};
-use std::sync::{Arc};
-use datafusion::arrow::array::{ArrayRef, Int32Array, StringArray, UInt64Builder, UInt8Builder};
+use std::sync::{Arc, Mutex};
+use datafusion::arrow::array::{ArrayBuilder, ArrayRef, Int32Array, StringArray, StringBuilder};
 use datafusion::arrow::datatypes::DataType;
 use datafusion::arrow::record_batch::RecordBatch;
 use datafusion::error::DataFusionError;
@@ -12,8 +12,9 @@ use datafusion::physical_plan::memory::MemoryStream;
 use ignite_rs::{Client, Ignite};
 use ignite_rs::cache::QueryEntity;
 use ignite_rs::error::IgniteError;
-use ignite_rs::protocol::{FLAG_COMPACT_FOOTER, FLAG_HAS_SCHEMA, FLAG_OFFSET_ONE_BYTE, read_i32, read_string, read_u16, read_u8, read_wrapped_data, TypeCode};
-use crate::arrow::datatypes::SchemaRef;
+use ignite_rs::protocol::{read_complex_obj_dyn, read_i32, read_string, read_u8, read_wrapped_data_dyn, TypeCode};
+use crate::arrow::array::PrimitiveBuilder;
+use crate::arrow::datatypes::{Int32Type, SchemaRef};
 use crate::dynamic_type::DynamicIgniteType;
 
 pub struct IgniteExec {
@@ -70,14 +71,13 @@ impl ExecutionPlan for IgniteExec {
     }
 
     fn execute(&self, _partition: usize, _context: Arc<TaskContext>) -> crate::Result<SendableRecordBatchStream> {
-        let mut _id_array = UInt8Builder::new(0);
-        let mut _account_array = UInt64Builder::new(0);
-
+        // Get the cache
         let mut client = self.client.clone();
         let cache = client
             .get_or_create_cache::<DynamicIgniteType, DynamicIgniteType>(&self.table_name)
             .map_err(|e| DataFusionError::Execution(e.to_string()) )?;
 
+        // Get the entity definition
         let entity = (|entities: &Option<Vec<QueryEntity>>| -> crate::Result<QueryEntity> {
             let entities = entities.as_ref()
                 .ok_or(DataFusionError::Internal("No entities!".to_string()))?;
@@ -88,63 +88,65 @@ impl ExecutionPlan for IgniteExec {
                 .ok_or(DataFusionError::Internal("No entities!".to_string()))?;
             Ok(entity.clone())
         })(&cache.cfg.query_entities)?;
-        for field in entity.query_fields.iter() {
-            println!("field={:?}", field);
-        }
 
+        let mut columns: Arc<Mutex<Vec<Box<dyn ArrayBuilder>>>> = Arc::new(Mutex::new(vec![]));
         cache.query_scan_dyn(1024, &mut |reader, count| {
-            // key
-            match entity.key_type.as_str() {
-                "java.lang.Int" => {
-                    let key_type_code = read_u8(reader)?; // 3 = i32
-                    if TypeCode::try_from(key_type_code)? != TypeCode::Int {
-                        Err(IgniteError::from("Invalid type!"))?
+
+            // allocate columns
+            for field in entity.query_fields.iter() {
+                println!("field={:?}", field);
+                match field.type_name.as_str() {
+                    "java.lang.Integer" => {
+                        let ar = Int32Array::builder(count as usize);
+                        columns.lock().unwrap().push(Box::new(ar));
                     }
-                    let region_key = read_i32(reader)?; // i32
-                },
-                _ => Err(IgniteError::from("Invalid type!"))?
-            }
-
-            // value
-            let val_type_code = read_u8(reader)?; // 27 = WrappedData
-            let wrapped_len = read_i32(reader)?;
-
-            let val_type = read_u8(reader)?; // 103 = Complex object
-            let ver = read_u8(reader)?; // 1
-            let flags = read_u16(reader)?; // 43
-            let type_id = read_i32(reader)?;
-            let hash_code = read_i32(reader)?;
-            let complex_obj_len = read_i32(reader)?; // 157
-            let schema_id = read_i32(reader)?;
-            let t2 = read_i32(reader)?; // 155
-            
-            let t3 = read_u8(reader)?; // 9 = Null
-            let name = read_string(reader)?;
-            println!("{} {}", name.len(), name);
-
-            let t5 = read_u8(reader)?; // 9 = Null
-            let comment = read_string(reader)?;
-            println!("{} {}", comment.len(), comment);
-
-            // Footer / schema
-            if flags & FLAG_HAS_SCHEMA != 0 {
-                if flags & FLAG_COMPACT_FOOTER != 0 {
-                    if flags & FLAG_OFFSET_ONE_BYTE != 0 {
-                        let field_count = 2;
-                        for _ in 0..field_count {
-                            let _ = read_u8(reader)?;
-                        }
-                    } else {
-                        todo!()
+                    "java.lang.String" => {
+                        let ar = StringBuilder::new(count as usize);
+                        columns.lock().unwrap().push(Box::new(ar));
                     }
-                } else {
-                    todo!()
+                    _ => todo!("Unknown field type: {}", field.type_name)
                 }
-            } else {
-                // no schema, nothing to do
             }
-            
-            let wrapped_offset = read_i32(reader)?;
+
+            // fill columns from rows
+            for _ in 0..count {
+                // key
+                match entity.key_type.as_str() {
+                    "java.lang.Integer" => {
+                        let key_type_code = read_u8(reader)?;
+                        match TypeCode::try_from(key_type_code)? {
+                            TypeCode::Int => {
+                                let mut cols = columns.lock().unwrap();
+                                let mut sb = cols.get_mut(0).unwrap().as_mut();
+                                let mut sb = sb.as_any_mut().downcast_mut::<PrimitiveBuilder<Int32Type>>().unwrap();
+                                let v = read_i32(reader)?;
+                                sb.append_value(v).unwrap();
+                            },
+                            _ => todo!("Unknown type: {}", key_type_code)
+                        }
+                    },
+                    _ => {
+                        let str = format!("Unknown type: {}", entity.key_type.as_str());
+                        Err(IgniteError::from(str.as_str()))?
+                    }
+                }
+
+                // value
+                read_wrapped_data_dyn(reader, &mut |reader, len| {
+                    read_complex_obj_dyn(reader, &mut |reader, len| {
+                        let t2 = read_i32(reader)?; // 155
+
+                        let t3 = read_u8(reader)?; // 9 = Null
+                        let name = read_string(reader)?;
+                        println!("{} {}", name.len(), name);
+
+                        let t5 = read_u8(reader)?; // 9 = Null
+                        let comment = read_string(reader)?;
+                        println!("{} {}", comment.len(), comment);
+                        Ok(())
+                    })
+                })?;
+            }
 
             Ok(())
         }).map_err(|e| DataFusionError::Execution(e.to_string()) )?;
